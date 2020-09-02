cdf9ab31d785432a034cecdc754b354336e02e4f
nqounet <mail@nqou.net>
2020-09-02 19:48:59 +0900

    init

---
+++ src/main/kotlin/net/nqou/kotlinspringrestfulsample/KotlinSpringRestfulSampleApplication.kt
@@ -0,0 +1,11 @@
+package net.nqou.kotlinspringrestfulsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestfulSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringRestfulSampleApplication>(*args)
+}
---
+++ src/test/kotlin/net/nqou/kotlinspringrestfulsample/KotlinSpringRestfulSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.nqou.kotlinspringrestfulsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringRestfulSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
